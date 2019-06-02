42379e10a8139ceb87e34f5e79a889e9698dabba
Kelly Muhindi <muhindi.kn@gmail.com>
2019-06-02 11:55:21 +0300

    Clean up for new section

--- src/com/luv2code/springdemo/HelloSpringApp.java
+++ src/com/luv2code/springdemo/HelloSpringApp.java
@@ -6,20 +6,17 @@ public class HelloSpringApp {
 
 	public static void main(String[] args) {
 		
-		//load the spring configuration file
+		//lo    ad the spring configuration file
 		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
 		
 		//retrieve bean from Spring container
-		CricketCoach theCoach = context.getBean("cricketCoach",CricketCoach.class);
-		Coach raceCoach = context.getBean("racingCoach",RacingCoach.class);
+		Coach trackCoach = context.getBean("myCoach",Coach.class);
 		
 		//call methods on the bean
-		System.out.println(theCoach.getDailyWorkout());
-		System.out.println(theCoach.getDailyFortune());
-		System.out.println(theCoach.getEmailAddress());
-		System.out.println(theCoach.getTeamName());
 		
-		System.out.println("Racing coach: " + raceCoach.getDailyWorkout());
+		System.out.println("Racing coach: " + trackCoach.getDailyWorkout());
+		System.out.println("Racing coach: " + trackCoach.getDailyFortune());
+		
 		//close the context
 		context.close();
 		
