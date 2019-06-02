137fc03ec1a90d9768d67df07d855532dc6030b7
Kelly Muhindi <muhindi.kn@gmail.com>
2019-06-02 12:12:53 +0300

    Add another context file and test singleton and prototype bean scopes

---
+++ src/com/luv2code/springdemo/BeanScopeDemoApp.java
@@ -0,0 +1,26 @@
+package com.luv2code.springdemo;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class BeanScopeDemoApp {
+
+	public static void main(String[] args) {
+		
+		//load spring config file
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("bean-scope-applicationContext.xml");
+		
+		//retrieve bean from spring container
+		Coach theCoach = context.getBean("myCoach", Coach.class);
+		Coach anotherCoach = context.getBean("myCoach", Coach.class);
+		
+		//check if they are the same bean
+		boolean result = (theCoach == anotherCoach);
+		System.out.println("Beans are same object? " + result);
+		
+		System.out.println("Memory location for theCoach-->" + theCoach + "\nMemory location for anotherCoach--> " + anotherCoach);
+		
+		//closae the context
+		context.close();
+	}
+
+}
